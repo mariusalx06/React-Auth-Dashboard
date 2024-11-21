be0c53407a949b772ba22a7aef591a3da85c3a07
Marius Calin <marius.calin0699@gmail.com>
2024-11-21 11:08:26 +0200

    Complete mobile friendly

--- app/components/style/Footer.js
+++ app/components/style/Footer.js
@@ -1,13 +1,21 @@
 "use client";
 
+import { useState, useEffect } from "react";
 import styles from "./Footer.module.css";
 
 export default function Footer() {
+  const [currentYear, setCurrentYear] = useState("");
+
+  useEffect(() => {
+    const year = new Date().getFullYear();
+    setCurrentYear(year);
+  }, []);
+
   return (
     <footer className={styles.footer}>
       <div className={styles.container}>
         <p className={styles.text}>
-          © 2024 Marius's Dashboard App. All Rights Reserved.
+          © {currentYear} Marius's Dashboard App. All Rights Reserved.
         </p>
       </div>
     </footer>
